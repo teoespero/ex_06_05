08f824dd5df86f83eaab596793a3f9baa23ca9cf
Teo Espero <38012835+teoespero@users.noreply.github.com>
2018-10-31 14:56:29 -0700

    Add files via upload

---
+++ ex-06-05.py
@@ -0,0 +1,59 @@
+# Teo Espero
+# BS Cloud and Systems Administration
+# Western Governors University
+
+# functions
+def printGreetings():
+    print('Exercise 06.05')
+    print('This program locates numbers on a string.')
+    print('------------------------------------------')
+
+def getUserAction():
+    global myString
+    proceed = True
+    while proceed:
+        yourAnswer = input('Do you want to enter a custom string (y/n)? ').lower()
+
+        if yourAnswer in 'yn':
+            proceed = False
+            if yourAnswer == 'y':
+                myString = input('Enter a string: ').lower()
+        else:
+            print('Invalid response. y/n only.')
+    return myString
+
+def getNumber():
+    number = ''
+    dot = 0
+    for piece in myString:
+        if piece in '0123456789.':
+            if piece == '.':
+                dot = dot + 1
+                if dot > 1:
+                    piece = ''
+        number = number + piece
+    return number
+
+# declarations
+defString = 'X-DSPAM-Confidence: 0.8457   '
+myString = defString
+
+# begin
+
+again = True
+while again:
+    printGreetings()
+    myString = getUserAction()
+    theNumber = getNumber()
+    print(theNumber)
+    while True:
+        tryAgain = input('Another run(y/n)? ').lower()
+        if tryAgain in 'yn':
+            break
+        else:
+            print('Invalid response. y/n only.')
+    if tryAgain == 'n':
+        again = False
+    myString = defString
+
+# end
